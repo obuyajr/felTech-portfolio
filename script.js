8e4e43eb68f4a4044d7780a8943cbab6b7d7553b
obuyajr <felix37brock@gmail.com>
2024-04-12 16:46:39 +0300

    link plot with a house

--- script.js
+++ script.js
@@ -18,8 +18,25 @@ hamburger.addEventListener('click', function () {
 
 function sendMail(){
 	var params = {
-		name : document.getElementById("name"),
-		email : document.getElementById("email"),
-		message : document.getElementById("message")
+		name : document.getElementById("name").value,
+		email : document.getElementById("email").value,
+		message : document.getElementById("message").value,
+	};
+
+
+const serviceID = "service_0prqz6j"
+const templateID = "service_0prqz6j"
+
+emailjs.send(serviceID,templateID,params)
+.then(
+	res=>{
+		document.getElementById("name").value = "";
+		document.getElementById("email").value = "";
+		document.getElementById("message").value = "";
+
+		console.log(res);
+		alert("Message Sent Successfully!!");
 	}
+)
+.catch((err)=>console.log(err));
 }
